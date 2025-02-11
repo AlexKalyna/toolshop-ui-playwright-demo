160656b9b8a74b49228b9368677520cbeb860e70
Oleksandr Kalyna <oleksandrkalyna@yahoo.com>
2025-02-11 21:32:31 +0200

    fixed order product test (#4)

---
+++ utils/paymentMethods.ts
@@ -0,0 +1,7 @@
+export enum PaymentMethods {
+  cash = 'Cash on Delivery',
+  payLater = 'Buy Now Pay Later',
+  giftCard = 'Gift Card',
+  creditCard = 'Credit Card',
+  bankTransfer = 'Bank Transfer'
+}
